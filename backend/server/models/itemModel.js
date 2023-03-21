e8e88db8c87891f48fab5f9d4cdcbc4aff2fd22c
Viky Belina <v_belina@salemstate.edu>
2023-03-20 21:33:49 -0400

    item Model

--- backend/server/models/itemModel.js
+++ backend/server/models/itemModel.js
@@ -1,8 +1,8 @@
 const mongoose = require("mongoose");
-const { Schema } = require("zod");
+const { Schema } = mongoose;
 
-//user schema/model
-const newItemSchema = new mongoose.Schema(
+// Item schema/model
+const newItemSchema = new Schema(
   {
     itemId: {
       type: String,
@@ -23,4 +23,4 @@ const newItemSchema = new mongoose.Schema(
   { collection: "Items" }
 );
 
-module.exports = mongoose.model('items', newItemSchema)
+module.exports = mongoose.model('items', newItemSchema);
