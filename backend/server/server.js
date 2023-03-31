9781fa4e165ff8a4694bb2632077997eaa893488
Viky Belina <v_belina@salemstate.edu>
2023-03-31 11:01:21 -0400

    server.js file updated

--- backend/server/server.js
+++ backend/server/server.js
@@ -9,10 +9,17 @@ const dbConnection = require('./config/db.config')
 const editUser = require('./routes/userEditUser')
 const deleteUser = require('./routes/userDeleteAll')
 
-const itemList = require('./routes/itemGetAllItems')
-const newItem = require('./routes/itemCreateItem')
-const getItemById = require('./routes/itemGetItemById')
-const deleteItem = require('./routes/itemDeleteAll')
+//ITEMS
+const itemList = require('./routes/item/itemGetAllItems')
+const newItem = require('./routes/item/itemCreateItem')
+const getItemById = require('./routes/item/itemGetItemById')
+const deleteAllItems = require('./routes/item/itemDeleteAll')
+const editItemRoute = require("./routes/item/itemEditItem") // Make sure the path is correct
+const deleteItemByIdRoute = require("./routes/item/itemDeleteById"); // Make sure to use the correct filename here
+
+//TICKETS
+const createNewTicketRouter = require("./routes/tickets/ticketCreateNew");
+const getAllTicketsRoute = require('./routes/tickets/ticketGetAll')
 
 require('dotenv').config();
 const SERVER_PORT = 8081
@@ -20,6 +27,8 @@ const SERVER_PORT = 8081
 dbConnection()
 app.use(cors({origin: '*'}))
 app.use(express.json())
+app.use(cors());
+
 app.use('/user', loginRoute)
 app.use('/user', registerRoute)
 app.use('/user', getAllUsersRoute)
@@ -27,11 +36,19 @@ app.use('/user', getUserByIdRoute)
 app.use('/user', editUser)
 app.use('/user', deleteUser)
 
+//ITEM
 app.use('/item', itemList)
 app.use('/item', newItem)
 app.use('/item', getItemById)
-app.use('/item', deleteItem)
+app.use('/item', deleteAllItems)
+app.use("/item", deleteItemByIdRoute);
+app.use("/item", editItemRoute);
+app.use("/item", require("./routes/item/itemEditItem"));
+app.use("/item", require("./routes/item/itemGetItemByDB"));
 
+//TICKETS
+app.use('/ticket', createNewTicketRouter)
+app.use('/ticket', getAllTicketsRoute)
 
 app.listen(SERVER_PORT, (req, res) => {
     console.log(`The backend service is running on port ${SERVER_PORT} and waiting for requests.`);
